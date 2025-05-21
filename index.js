0ba49ff83f0f7839852d417fcfdb88f5c140703e
laluoki <you@example.com>
2025-05-21 16:14:13 +0800

    [backend] "add any API  for user & device"

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(cors());
 
 const UserController = require("./controllers/userController");
 const DeviceController = require("./controllers/deviceController");
+const middleware = require("./middleware/auth");
 
 app.get("/", (req, res) => {
   res.status(200).send({
@@ -19,10 +20,29 @@ app.get("/", (req, res) => {
 });
 // User
 app.post("/api/v1/register/user", UserController.createUser);
-app.get("/api/v1/getAll/user", UserController.getAllUser);
+app.post(
+  "/api/v1/login/user",
+  UserController.login,
+  middleware.authentication,
+  middleware.isAdmin
+);
+app.get(
+  "/api/v1/getAll/user",
+  UserController.getAllUser,
+  middleware.authentication,
+  middleware.isAdmin
+);
+app.delete(
+  "/api/v1/delete/user/:id",
+  UserController.deleteUser,
+  middleware.authentication,
+  middleware.isAdmin
+);
 
 // Device
 app.post("/api/v2/register/device", DeviceController.addDevice);
+app.put("/api/v2/update/device/:id", DeviceController.updateDevice);
+app.delete("/api/v2/delete/device/:id", DeviceController.deletedDevice);
 app.get("/api/v2/getAll/device", DeviceController.getAllDevice);
 app.post("/api/v2/add/device/list", DeviceController.addDeviceToList);
 
