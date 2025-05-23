5644bf708980588a326cb25c354b4c5e85a8f9a9
laluoki <you@example.com>
2025-05-23 16:30:01 +0800

    [backend] adding any API

--- index.js
+++ index.js
@@ -11,9 +11,68 @@ app.use(cors());
 
 const UserController = require("./controllers/userController");
 const DeviceController = require("./controllers/deviceController");
+const PerformanceController = require("./controllers/performanceController");
 const middleware = require("./middleware/auth");
+const { device } = require("./models");
+const { performance } = require("./models");
 
-app.get("/", (req, res) => {
+app.get("/seed", async (req, res) => {
+  await device.bulkCreate([
+    // { samId: "SAM01", speed: 38 },
+    // { samId: "SAM01", speed: 38 },
+    // { samId: "SAM02", speed: 20 },
+    // { samId: "SAM02", speed: 99 },
+    // { samId: "SAM03", speed: 38 },
+    // { samId: "SAM03", speed: 99 },
+    {
+      samId: "SAM01",
+      deviceIP: "192.121.22.1",
+      deviceUsername: "admin",
+      devicePassword: "Admin1234",
+      deviceRootFolder: "/A",
+      cameraIP: "192.121.22.1",
+      cameraUsername: "root",
+      cameraPassword: "Admin1234",
+      cameraRootFolder: "/A/a",
+      cameraType: "sony",
+      location: "amman",
+      speed: 40,
+    },
+    {
+      samId: "SAM02",
+      deviceIP: "192.121.22.1",
+      deviceUsername: "admin",
+      devicePassword: "Admin1234",
+      deviceRootFolder: "/A",
+      cameraIP: "192.121.22.1",
+      cameraUsername: "root",
+      cameraPassword: "Admin1234",
+      cameraRootFolder: "/A/a",
+      cameraType: "sony",
+      location: "amman",
+      speed: 99,
+    },
+    {
+      samId: "SAM03",
+      deviceIP: "192.121.22.1",
+      deviceUsername: "admin",
+      devicePassword: "Admin1234",
+      deviceRootFolder: "/A",
+      cameraIP: "192.121.22.1",
+      cameraUsername: "root",
+      cameraPassword: "Admin1234",
+      cameraRootFolder: "/A/a",
+      cameraType: "sony",
+      location: "amman",
+      speed: 99,
+    },
+  ]);
+  res.status(200).send({
+    message: "Successfully",
+  });
+});
+app.get("/speed", async (req, res) => {
+  await performance.bulkCreate([{ speed: 38 }, { speed: 38 }, { speed: 38 }]);
   res.status(200).send({
     message: "Successfully",
   });
@@ -32,6 +91,11 @@ app.get(
   middleware.authentication,
   middleware.isAdmin
 );
+app.put(
+  "/api/v1/update/user/:id",
+  UserController.updateUser,
+  middleware.authentication
+);
 app.delete(
   "/api/v1/delete/user/:id",
   UserController.deleteUser,
@@ -42,10 +106,14 @@ app.delete(
 // Device
 app.post("/api/v2/register/device", DeviceController.addDevice);
 app.put("/api/v2/update/device/:id", DeviceController.updateDevice);
-app.delete("/api/v2/delete/device/:id", DeviceController.deletedDevice);
 app.get("/api/v2/getAll/device", DeviceController.getAllDevice);
+app.delete("/api/v2/delete/device/:id", DeviceController.deletedDevice);
 app.post("/api/v2/add/device/list", DeviceController.addDeviceToList);
+app.get("/api/v2/get/device/performance", DeviceController.getPerformance);
+app.get("/api/v2/get/device/overspeed/:id", DeviceController.getOverSpeed);
 
+// Performance
+app.get("/api/v3/get/perfor", PerformanceController.getPerformance);
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`listening on http://localhost:${PORT}`);
 });
